ee31344c925a356683496b1509b62cad3e8d2b45
faz <ahmedfayez834@gmail.com>
2022-07-14 10:21:50 +0200

    home

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,23 +1,53 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget
-{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(
           Icons.menu,
         ),
-        title: const Text(
-          'Update'
-        ),
-        actions: const [
-          Icon(
-            Icons.notification_important,
+        title: const Text('Update'),
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: const Icon(
+                Icons.notification_important,
+              ),
+          ),
+          IconButton(
+              onPressed: (){},
+              icon: const Icon(
+                Icons.search,
+              ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            child: Text(
+              'new_11',
+              style: TextStyle(
+                // color: Colors.red,
+              ),
+            ),
+          ),
+          Text(
+            'new_11',
+            style: TextStyle(
+              // color: Colors.red,
+            ),
+          ),
+          Text(
+            'new_1122',
+            style: TextStyle(
+              color: Colors.red,
+            ),
           ),
         ],
       ),
