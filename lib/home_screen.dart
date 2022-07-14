39ef7307806a2f2958baebf5fbb4fd102022859e
faz <ahmedfayez834@gmail.com>
2022-07-14 11:54:53 +0200

    home

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -28,29 +29,130 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            height: 100,
-            child: Text(
-              'new_11',
-              style: TextStyle(
-                // color: Colors.red,
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadiusDirectional.only(
+                  topStart: Radius.circular(20),
+                ),
               ),
-            ),
-          ),
-          Text(
-            'new_11',
-            style: TextStyle(
-              // color: Colors.red,
-            ),
-          ),
-          Text(
-            'new_1122',
-            style: TextStyle(
-              color: Colors.red,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  const Image(
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                    image:NetworkImage('https://i.ytimg.com/vi/ekgUjyWe1Yc/maxresdefault.jpg'),
+                  ),
+                  Container(
+                    width: 200,
+                    color: Colors.black.withOpacity(.5),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                    ),
+                    child: const Text(
+                      'car11',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
+      // body: Container(
+      //   color: Colors.amberAccent,
+      //   width: double.infinity,
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       mainAxisSize: MainAxisSize.max,
+      //       // mainAxisAlignment: MainAxisAlignment.center,
+      //       // crossAxisAlignment: CrossAxisAlignment.end,
+      //       children: [
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           child: Text(
+      //             'new_11',
+      //             style: TextStyle(
+      //               // color: Colors.red,
+      //             ),
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
